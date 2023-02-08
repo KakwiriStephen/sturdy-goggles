819965aa5b650e300b8f91f133c403cc56c2e629
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-08 16:58:21 +0300

    About

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-import Me from "../../assets/mee.png";
+import Me from "../../assets/mee2.png";
 import { FaAward } from "react-icons/fa";
 import { FiUsers } from "react-icons/fi";
 import { AiFillFolderOpen } from "react-icons/ai";
