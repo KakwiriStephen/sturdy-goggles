e92d932e8d107dbd086b3bbd3cac8e8d0fbcc3f2
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-08 15:32:36 +0300

    Nav

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,8 +1,33 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./nav.css";
 
+import { AiOutlineHome } from "react-icons/ai";
+import { FiUserPlus } from "react-icons/fi";
+import { BsFillBookmarksFill } from "react-icons/bs";
+import { RiServiceLine } from "react-icons/ri";
+import { BiMessageDetail } from "react-icons/bi";
+
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <a href="#">
+        <AiOutlineHome />
+      </a>
+      <a href="#about ">
+        <FiUserPlus />
+      </a>
+      <a href="experience">
+        <BsFillBookmarksFill />
+      </a>
+      <a href="services">
+        <RiServiceLine />
+      </a>
+      <a href="contact">
+        <BiMessageDetail />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
