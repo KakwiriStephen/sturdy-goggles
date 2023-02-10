786356aea8e0d079b41b4a3f921548b9d46b5d53
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-10 18:49:22 +0300

    Contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./contact.css";
 import { HiMail } from "react-icons/hi";
 import { BsWhatsapp } from "react-icons/bs";
+import { IoMdCall } from "react-icons/io";
 
 const Contact = () => {
   return (
@@ -12,7 +13,7 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <HiMail />
+            <HiMail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>ngottakakwiri@gmail.com</h5>
             <a
@@ -24,7 +25,7 @@ const Contact = () => {
             </a>
           </article>
           <article className="contact__option">
-            <HiMail />
+            <IoMdCall className="contact__option-icon" />
             <h4>Call</h4>
             <h5>+254 715 378 281</h5>
             <a href="tel:+254715378281" target="_blank" rel="noreferrer">
@@ -32,11 +33,11 @@ const Contact = () => {
             </a>
           </article>
           <article className="contact__option">
-            <BsWhatsapp />
+            <BsWhatsapp className="contact__option-icon" />
             <h4>WhatsApp </h4>
             <h5>kakwiri</h5>
             <a
-              href="https://api.whatsapp.com/send?phone=+254715378281"
+              href="https://wa.me/+254715378281"
               target="_blank"
               rel="noreferrer"
             >
