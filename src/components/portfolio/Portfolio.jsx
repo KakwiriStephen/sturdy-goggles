040eeb1eb8b3ab94cc537d9330e1586def8dc3b8
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-10 10:44:08 +0300

    Porfolio Array

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,59 @@
 import React from "react";
 import "./portfolio.css";
 
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.png";
+
+//Array
+const data = [
+  {
+    id: 1,
+    Image: IMG1,
+    title: "Crypto Currency Dashbord & Financial Visualization",
+    github: "https://github.com",
+    demo: "https://dribble.com",
+  },
+  {
+    id: 1,
+    Image: IMG2,
+    title: "Crypto Currency Dashbord & Financial Visualization",
+    github: "https://github.com",
+    demo: "https://dribble.com",
+  },
+  {
+    id: 1,
+    Image: IMG3,
+    title: "Crypto Currency Dashbord & Financial Visualization",
+    github: "https://github.com",
+    demo: "https://dribble.com",
+  },
+  {
+    id: 1,
+    Image: IMG4,
+    title: "Crypto Currency Dashbord & Financial Visualization",
+    github: "https://github.com",
+    demo: "https://dribble.com",
+  },
+  {
+    id: 1,
+    Image: IMG5,
+    title: "Crypto Currency Dashbord & Financial Visualization",
+    github: "https://github.com",
+    demo: "https://dribble.com",
+  },
+  {
+    id: 1,
+    Image: IMG6,
+    title: "Crypto Currency Dashbord & Financial Visualization",
+    github: "https://github.com",
+    demo: "https://dribble.com",
+  },
+];
+
 const Portfolio = () => {
   return <div>Portfolio</div>;
 };
