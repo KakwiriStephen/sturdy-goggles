23612d1d1a7074726f0d8366fc85bb1c981c17eb
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-10 11:40:57 +0300

    Testimonials

--- src/components/testimonials/Testimonial.jsx
+++ src/components/testimonials/Testimonial.jsx
@@ -15,14 +15,14 @@ const Testimonial = () => {
         <article className="testimonial">
           <div className="client__avater">
             <img src={AVTR1} alt="Avatar" />
-            <h5 className="client__name">Earnest Achieng</h5>
-            <small>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-              voluptas sint totam assumenda accusantium quia architecto nulla.
-              Quasi, totam pariatur voluptatem, eveniet iste quisquam eum
-              deserunt veritatis ab magnam quia?
-            </small>
           </div>
+          <h5 className="client__name">Earnest Achieng</h5>
+          <small>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+            voluptas sint totam assumenda accusantium quia architecto nulla.
+            Quasi, totam pariatur voluptatem, eveniet iste quisquam eum deserunt
+            veritatis ab magnam quia?
+          </small>
         </article>
       </div>
     </section>
