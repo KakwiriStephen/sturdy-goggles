c82a0de057e1dbfcf3ffcbb63763fb8c451238d9
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-11 14:09:32 +0300

    Mailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./contact.css";
 import { HiMail } from "react-icons/hi";
 import { BsWhatsapp } from "react-icons/bs";
 import { IoMdCall } from "react-icons/io";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -47,7 +69,7 @@ const Contact = () => {
         </div>
 
         {/* End of contact options */}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder="Your Name" required />
           <input type="text" name="email" placeholder="Your Email" required />
           <textarea
